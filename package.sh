9af59aabfada6b41340f8748a205b01a0fe9c464
Chayim I. Kirshen <c@kirshen.com>
2022-06-13 09:47:29 +0300

    Fixing arch

--- package.sh
+++ package.sh
@@ -13,12 +13,12 @@ rm -rf build *.rpm
 mkdir -p build/usr/bin
 cp deps/redis*/* build/usr/bin
 chmod 0755 build/usr/bin/*
+set -x
 fpm \
     -s dir \
     -C build \
     -n redis-tools \
     --version ${VERSION} \
-    --directories /usr/bin \
     --maintainer "oss@redis.com" \
     --license "BSD-3-Clause" \
     --url "https://redis.io" \
