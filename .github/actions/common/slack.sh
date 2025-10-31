2a74a11c28f277000da4540c16e9188a0203c00f
Petar Shtuchkin <petar.shtuchkin@redis.com>
2025-10-31 14:12:24 +0200

    Release automation: initial version for 8.2

---
+++ .github/actions/common/slack.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+slack_format_success_message() {
+jq --arg release_tag "$1" --arg url_prefix "$2" --arg footer "$3" --arg env "$4" '
+{
+  "icon_emoji": ":redis-circle:",
+  "text": (":package: RPM Packages Published for Redis: " + $release_tag + " (" + $env + ")"),
+  "blocks": (
+    [
+      {
+        "type": "header",
+        "text": { "type": "plain_text", "text": (":package: RPM Packages Published for Release " + $release_tag + " (" + $env + ")") }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "The following packages have been published:"
+        }
+      }
+    ] +
+    (
+      to_entries
+      | map({
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": (
+              "Distribution: *" + .key + "*\n" +
+              (
+                .value
+                | map("• " + .)
+                | join("\n")
+              )
+            )
+          }
+        })
+    ) +
+    [
+      {
+        "type": "context",
+        "elements": [
+          { "type": "mrkdwn", "text": $footer }
+        ]
+      }
+    ]
+  )
+}'
+}
+
+slack_format_failure_message() {
+    header=$1
+    workflow_url=$2
+    footer=$3
+    if [ -z "$header" ]; then
+        header=" "
+    fi
+    if [ -z "$footer" ]; then
+        footer=" "
+    fi
+
+# Create Slack message payload
+    cat << EOF
+{
+"icon_emoji": ":redis-circle:",
+"text": "$header",
+"blocks": [
+    {
+    "type": "header",
+    "text": {
+        "type": "plain_text",
+        "text": "❌  $header"
+    }
+    },
+    {
+    "type": "section",
+    "text": {
+        "type": "mrkdwn",
+        "text": "Workflow run: $workflow_url"
+    }
+    },
+    {
+    "type": "context",
+    "elements": [
+        {
+        "type": "mrkdwn",
+        "text": "$footer"
+        }
+    ]
+    }
+]
+}
+EOF
+}
+
